fb46bdcd21c53aedd42f55bb9f60209a1d3ab2d0
blakem3512 <142925630+blakem3512@users.noreply.github.com>
2025-04-23 20:41:24 -0400

    Update PitcherTeamApp.java
    If there's an easier way to import all of these without having so much text, please let me know :) otherwise, I'll take out the unused ones when I finish the GUI.

--- pitcherteamapp/PitcherTeamApp.java
+++ pitcherteamapp/PitcherTeamApp.java
@@ -1,9 +1,19 @@
 package com.mycompany.pitcherteamapp;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import java.util.List;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 
