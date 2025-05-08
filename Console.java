0e79854815c77c871a4fbacfafd20bed6756df54
0575602 <philih6580@students.clarkstate.edu>
2025-05-08 15:31:10 -0400

    Update Console.java

--- Console.java
+++ Console.java
@@ -1,3 +1,17 @@
+/*
+    Created by:    Hilary Philistin,
+                   Matthew Blake, 
+                   Henry Sesay, 
+                   Braden Henry, 
+                   Zach Thompson, 
+                   Wyatt Metcalf
+    Created on:    05/08/2025
+    Team’s name:   Pitcher
+    Description:   Create a baseball statistics program that produce a report's 
+                   list of the statistics for all pitchers in that game and 
+                   calculates the earned run average for each pitcher.
+*/
+
 package com.mycompany.pitcherteamapp;
 
 import java.util.Scanner;
